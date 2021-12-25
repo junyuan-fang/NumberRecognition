b20da78fc49c9aba92cbc286cde632a2e64ef86c
JY Fang <junyuan.fang@helsinki.fi>
2021-12-25 17:02:16 +0200

    pylint and format

--- tasks.py
+++ tasks.py
@@ -14,6 +14,15 @@ def test(ctx):
     ctx.run("pytest src")
 
 @task
+def lint(ctx):
+    ctx.run("pylint src")
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src")
+
+
+@task
 def start(ctx):
     ctx.run("python3 src/main.py")
 
