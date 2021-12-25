7c1cb8ac7458f7b29e65bcc25683f2990c992cec
JY Fang <junyuan.fang@helsinki.fi>
2021-12-25 23:31:38 +0200

    test of _load_img and _load_label

--- src/repositories/mnist.py
+++ src/repositories/mnist.py
@@ -1,25 +1,24 @@
-# This file is for loading all mnist and boolean values
+#This file is for loading all mnist and boolean values 
 import gzip
 import pickle
 import os
 import numpy as np
 
 key_file = {
-    'train_img': 'train-images-idx3-ubyte.gz',
-    'train_label': 'train-labels-idx1-ubyte.gz',
-    'test_img': 't10k-images-idx3-ubyte.gz',
-    'test_label': 't10k-labels-idx1-ubyte.gz'
+    'train_img':'train-images-idx3-ubyte.gz',
+    'train_label':'train-labels-idx1-ubyte.gz',
+    'test_img':'t10k-images-idx3-ubyte.gz',
+    'test_label':'t10k-labels-idx1-ubyte.gz'
 }
 train_num = 60000
 test_num = 10000
 img_dim = (1, 28, 28)
-img_size = 28*28  # 784
+img_size = 28*28 #784
 GRAYSCALE = 73
 
-# /home/fjunyuan/Codes/TiraLabra/Recognition_of_handwritten_numbers/src/MNIST_data/
-#os.path.abspath(os.path.dirname(os.getcwd())) + "/Recognition_of_handwritten_numbers/src/MNIST_data"
+#/home/fjunyuan/Codes/TiraLabra/Recognition_of_handwritten_numbers/src/MNIST_data/
 dataset_dir = "./src/MNIST_data"
-# print(dataset_dir)
+#os.path.abspath(os.path.dirname(os.getcwd())) + "/Recognition_of_handwritten_numbers/src/MNIST_data"
 save_file_label = dataset_dir + "/label.pkl"
 save_file_img = dataset_dir + "/test_img_bit.pkl"
 save_file_img_boolean = dataset_dir + "/img_boolean.pkl"
@@ -33,20 +32,18 @@ def _load_img(file_name):
     Returns:
         data: returns data bits, 60000x784 or  10000x784
     """
-
+    
     file_path = dataset_dir + "/" + file_name
-
-    print("Converting " + file_name + " to NumPy Array ...")
+    
+    print("Converting " + file_name + " to NumPy Array ...")    
     with gzip.open(file_path, 'rb') as f:
-        # according to the MNIST
-        data = np.frombuffer(f.read(), np.uint8, offset=16)
-    # -1 then we do not need to know the original size. train = 60000x784, test = 10000x784
-    data = data.reshape(-1, img_size)
+            data = np.frombuffer(f.read(), np.uint8, offset=16)#according to the MNIST
+    data = data.reshape(-1, img_size)#-1 then we do not need to know the original size. train = 60000x784, test = 10000x784
     print("Done")
+    
 
-    # print(data.shape)
+    #print(data.shape)
     return data
-
 
 def _load_label(file_name):
     """ Read one label .gz file. (Dataset read from MNIST_data folder)
@@ -60,28 +57,24 @@ def _load_label(file_name):
     print(file_path)
     print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
-        # according to the MNIST
-        labels = np.frombuffer(f.read(), np.uint8, offset=8)
+            labels = np.frombuffer(f.read(), np.uint8, offset=8)#according to the MNIST
     print("Done")
 
-    # print(data.shape)
+    #print(data.shape)
     return labels
-
-
 def init_label():
     """ Creating pickle file for label
         Save as {'train_label': ... , 'test_label': ...}
     """
 
     dataset = {}
-    dataset['train_label'] = _load_label(key_file['train_label'])
-    dataset['test_label'] = _load_label(key_file['test_label'])
+    dataset ['train_label'] = _load_label(key_file['train_label'])
+    dataset ['test_label'] = _load_label(key_file['test_label'])
 
     print("Creating pickle file for label...")
     with open(save_file_label, 'wb') as f:
-        pickle.dump(dataset, f, -1)  # for later loading
+        pickle.dump(dataset, f, -1)#for later loading
     print("Done!")
-
 
 def _load_label_pkl():
     """ If pickle file not exist, create it.
@@ -96,14 +89,12 @@ def _load_label_pkl():
         dataset = pickle.load(f)
 
     # value = dataset['train_img'][9]
-
+    
     # for w in value:
-    #     print(w)
+    #     print(w) 
 
     return dataset
-# ------------------------------------------------------
-
-
+#------------------------------------------------------
 def init_test_img():
     """ Creating pickle file for test img
         Save as {'train_img': ... , 'test_img': ...}
@@ -114,9 +105,8 @@ def init_test_img():
 
     print("Creating pickle file for test image...")
     with open(save_file_img, 'wb') as f:
-        pickle.dump(dataset, f, -1)  # for later loading
+        pickle.dump(dataset, f, -1)#for later loading
     print("Done!")
-
 
 def load_test_img():
     """ If pickle file not exist, load .gz, and create pickle file.
@@ -129,12 +119,10 @@ def load_test_img():
         init_test_img()
 
     with open(save_file_img, 'rb') as f:
-        dataset = pickle.load(f)  # slow
+        dataset = pickle.load(f)# slow
 
     return dataset
-# ------------------------------------------------------
-
-
+#------------------------------------------------------
 def conver_img_to_boolean(img_set, grayScale=73):
     """ Go through 60000x28x28 or 10000x28x28
         turn value 0-255 to 0-1
@@ -149,19 +137,18 @@ def conver_img_to_boolean(img_set, grayScale=73):
     set = []
     for img in img_set:
         new_img = img.reshape(28, 28)
-        matrix = []
+        matrix = [] 
         for row in new_img:
             list = []
             for x in range(len(row)):
-                if row[x] <= grayScale:
+                if row[x]<=grayScale:
                     list.append(0)
                 else:
                     list.append(1)
             matrix.append(list)
         set.append(matrix)
-
+    
     return set
-
 
 def init_img_boolean(grayScale=73):
     """ Creating pickle file for img
@@ -172,14 +159,13 @@ def init_img_boolean(grayScale=73):
     train_img = _load_img(key_file['train_img'])
     test_img = _load_img(key_file['test_img'])
 
-    dataset['train_img'] = conver_img_to_boolean(train_img, grayScale)
-    dataset['test_img'] = conver_img_to_boolean(test_img, grayScale)
+    dataset ['train_img'] = conver_img_to_boolean(train_img,grayScale)
+    dataset ['test_img'] = conver_img_to_boolean(test_img,grayScale)
 
     print("Creating pickle file for image(boolean)...")
     with open(save_file_img_boolean, 'wb') as f:
-        pickle.dump(dataset, f, -1)  # for later loading
+        pickle.dump(dataset, f, -1)#for later loading
     print("Done!")
-
 
 def load_img_boolean(grayScale=73):
     """ If pickle file not exist, load .gz, turn value 0-255 to 0-1 with grayscale and create pickle file.
@@ -192,18 +178,16 @@ def load_img_boolean(grayScale=73):
         init_img_boolean(grayScale)
 
     with open(save_file_img_boolean, 'rb') as f:
-        dataset = pickle.load(f)  # takes 3s to load, which is slow
+        dataset = pickle.load(f)#takes 3s to load, which is slow
 
     # value = dataset['train_img'][9]
-
+    
     # for w in value:
-    #     print(w)
+    #     print(w) 
 
     return dataset
-# ------------------------------------------------------
-
-
-def conver_img_to_y_x(img_set, grayScale=73):
+#------------------------------------------------------
+def conver_img_to_y_x(img_set,grayScale=73):
     """ Go through 60000x28x28 or 10000x28x28
         find grayscale which are greater than parameter
         append it to the list 60000 x unknow.
@@ -220,13 +204,12 @@ def conver_img_to_y_x(img_set, grayScale=73):
         matrix = []
         for y in range(28):
             for x in range(28):
-                if new_img[y][x] <= grayScale:
+                if new_img[y][x]<=grayScale:
                     continue
                 else:
-                    matrix.append((y, x))
+                    matrix.append((y,x))
         set.append(matrix)
     return set
-
 
 def init_img_to_y_x(grayScale=73):
     """ Creating pickle file for img
@@ -237,12 +220,12 @@ def init_img_to_y_x(grayScale=73):
     train_img = _load_img(key_file['train_img'])
     test_img = _load_img(key_file['test_img'])
 
-    dataset['train_img'] = conver_img_to_y_x(train_img, grayScale)
-    dataset['test_img'] = conver_img_to_y_x(test_img, grayScale)
+    dataset ['train_img'] = conver_img_to_y_x(train_img, grayScale)
+    dataset ['test_img'] = conver_img_to_y_x(test_img, grayScale)
 
     print("Creating pickle file for image(index)...")
     with open(save_file_img_boolean_index, 'wb') as f:
-        pickle.dump(dataset, f, -1)  # for later loading
+        pickle.dump(dataset, f, -1)#for later loading
     print("Done!")
 
 
@@ -257,7 +240,7 @@ def load_img_to_y_x_index(grayScale=73):
 
     with open(save_file_img_boolean_index, 'rb') as f:
         dataset = pickle.load(f)
-
+    
     # matrix = [['#']*28 for i in range(28)]
     # value = dataset['train_img'][9]
     # for q in value:
@@ -265,7 +248,7 @@ def load_img_to_y_x_index(grayScale=73):
     #     x = q[1]
     #     matrix[y][x] = '.'
     # for w in matrix:
-    #     print(w)
+    #     print(w) 
     # print(dataset['train_label'][9])
 
     return dataset
@@ -274,8 +257,8 @@ def load_img_to_y_x_index(grayScale=73):
 if __name__ == '__main__':
     dataset = _load_label_pkl()
     print(dataset['train_label'][0])
-    load_img_boolean()
-    load_img_to_y_x_index()  # only value "1" location in the list
+    load_img_boolean() 
+    load_img_to_y_x_index()#only value "1" location in the list
 
     # #tests
     # dataset = {}
@@ -285,11 +268,11 @@ if __name__ == '__main__':
     # #print(dataset['train_img'][0])
     # print(os.path.abspath(__file__))
 
-    # img = dataset['train_img'][9]    # shape:[1,28,28]
-    # img = img.reshape(28, 28)                   # shape:[28,28]
+    # img = dataset['train_img'][9]    # shape:[1,28,28] 
+    # img = img.reshape(28, 28)                   # shape:[28,28]  
     # #imaging 0-127 are -128 - -1  negative
     # #      128-255 are    0 - 127 positive
-    # img2 = img * 0xff      # outline/恢复灰度值大小
+    # img2 = img * 0xff      # outline/恢复灰度值大小 
     # from pylab import *
     # import matplotlib.pyplot as plt
     # imshow(img, 'gray')
--- src/tests/repositories/mnist_test.py
+++ src/tests/repositories/mnist_test.py
@@ -1,13 +1,21 @@
 import unittest
-from repositories.mnist import _load_img
+import repositories.mnist as mnist
 # mnist.test_imported()
 
 
 class TestMnist(unittest.TestCase):
-    def test_train_img_data_num_train(self):
-        self.dataset = _load_img("train-images-idx3-ubyte.gz")
+    def test_load_train_img(self):
+        self.dataset = mnist._load_img("train-images-idx3-ubyte.gz")
         self.assertEqual(len(self.dataset), 60000)
 
-    def test_train_img_data_num_test(self):
-        self.dataset = _load_img("t10k-images-idx3-ubyte.gz")
+    def test_load_test_img(self):
+        self.dataset = mnist._load_img("t10k-images-idx3-ubyte.gz")
+        self.assertEqual(len(self.dataset), 10000)
+
+    def test_load_train_label(self):
+        self.dataset = mnist._load_label("train-labels-idx1-ubyte.gz")
+        self.assertEqual(len(self.dataset), 60000)
+
+    def test_load_test_label(self):
+        self.dataset = mnist._load_label("t10k-labels-idx1-ubyte.gz")
         self.assertEqual(len(self.dataset), 10000)
