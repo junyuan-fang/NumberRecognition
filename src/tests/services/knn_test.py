9d2b195abbe1e5e14d76d5752b094f3e1e4f29c9
JY Fang <junyuan.fang@helsinki.fi>
2021-12-27 00:07:36 +0200

    test of knn

--- src/tests/services/knn_test.py
+++ src/tests/services/knn_test.py
@@ -37,6 +37,65 @@ class TestKnn(unittest.TestCase):
         self.assertEqual(type(dist1), float)
 
     def test_percentage(self):
-        percentage = knn.percentage(5,100)
-        self.assertTrue(0.0<=percentage)
-        self.assertTrue(percentage<=1)
+        percentage = knn.percentage(10,50,3,"D22")
+        self.assertTrue(0.0<=percentage and percentage<=1)
+        percentage = knn.percentage(10,50,3,"D23")
+        self.assertTrue(0.0<=percentage and percentage<=1)
+
+    def test_get_dataB_normal_range_correct(self):
+        range = 10
+        dataB = knn._get_dataB_normal(range)
+        self.assertEqual(len(dataB[0]),range)
+        self.assertEqual(len(dataB[1]),range)
+    
+    def test_update_nearest_neighbour_correctly_when_heap_is_full(self):
+        k = 3
+        neighbour1 = (0.9,8)
+        neighbour2 = (0.3,2)
+        neighbour3 = (0.2,1)
+        neighbour4 = (0.4,3)
+        maxheap = [neighbour1,neighbour2,neighbour3]
+        knn.update_nearest_neighbour(k, maxheap, neighbour4[0], neighbour4[1])
+        new_max_dist_label = maxheap[0][1]
+        self.assertEqual(new_max_dist_label, neighbour4[1])
+        
+    def test_update_nearest_neighbour_correctly_when_heap_is_not_full(self):
+        k = 3
+        neighbour2 = (0.3,2)
+        neighbour3 = (0.2,1)
+        neighbour4 = (0.4,3)
+        maxheap = [neighbour2,neighbour3]
+        knn.update_nearest_neighbour(k, maxheap, neighbour4[0], neighbour4[1])
+        new_max_dist_label = maxheap[0][1]
+        self.assertEqual(new_max_dist_label, neighbour4[1])
+
+    def test_get_result_select_by_min_dist_when_all_neighbors_are_different(self):
+        neighbour1 = (0.9,8)
+        neighbour2 = (0.3,2)
+        neighbour3 = (0.2,1)
+        maxheap = [neighbour1,neighbour2,neighbour3]
+        result = knn.get_result(maxheap)
+        self.assertEqual(result,1)
+
+    def test_get_result_select_by_majority(self):
+        neighbour1 = (0.9,8)
+        neighbour2 = (0.3,8)
+        neighbour3 = (0.2,2)
+        maxheap = [neighbour1,neighbour2,neighbour3]
+        result = knn.get_result(maxheap)
+        self.assertEqual(result,8)
+
+    def test_recognition_returns_int(self):
+        result = knn.recognition()
+        self.assertEqual(type(result),int)
+    
+    def test_recognition_returns_valid_result(self):
+        result = knn.recognition()
+        self.assertTrue(result<=9)
+        self.assertTrue(result>=0)
+
+    def test_get_test_img_method_get_img_with_index_success(self):
+        i = 0
+        result = knn.get_test_img(i)
+        self.assertEqual(len(result),28)
+        self.assertEqual(len(result[0]),28)
