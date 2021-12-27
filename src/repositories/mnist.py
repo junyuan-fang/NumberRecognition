098af5ef28ec006ce08314188923f36df2c4ccbf
JY Fang <junyuan.fang@helsinki.fi>
2021-12-27 13:43:27 +0200

    better style in mnist.py

--- src/repositories/mnist.py
+++ src/repositories/mnist.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name
 #This file is for loading all mnist and boolean values
 import gzip
 import pickle
@@ -35,8 +36,8 @@ def _load_img(file_name):
     file_path = DATASET_DIR + "/" + file_name
 
     print("Converting " + file_name + " to NumPy Array ...")
-    with gzip.open(file_path, 'rb') as f:
-        data = np.frombuffer(f.read(), np.uint8, offset=16)#according to the MNIST
+    with gzip.open(file_path, 'rb') as file:
+        data = np.frombuffer(file.read(), np.uint8, offset=16)#according to the MNIST
     #-1 then we do not need to know the original size. train = 60000x784, test = 10000x784
     data = data.reshape(-1, IMG_SIZE)
     print("Done")
@@ -55,8 +56,8 @@ def _load_label(file_name):
     file_path = DATASET_DIR + "/" + file_name
     print(file_path)
     print("Converting " + file_name + " to NumPy Array ...")
-    with gzip.open(file_path, 'rb') as f:
-        labels = np.frombuffer(f.read(), np.uint8, offset=8)#according to the MNIST
+    with gzip.open(file_path, 'rb') as file:
+        labels = np.frombuffer(file.read(), np.uint8, offset=8)#according to the MNIST
     print("Done")
 
     #print(data.shape)
@@ -71,8 +72,8 @@ def init_label():
     dataset ['test_label'] = _load_label(key_file['test_label'])
 
     print("Creating pickle file for label...")
-    with open(SAVE_LABEL_DIR, 'wb') as f:
-        pickle.dump(dataset, f, -1)#for later loading
+    with open(SAVE_LABEL_DIR, 'wb') as file:
+        pickle.dump(dataset, file, -1)#for later loading
     print("Done!")
 
 def _load_label_pkl():
@@ -85,8 +86,8 @@ def _load_label_pkl():
     if not os.path.exists(SAVE_LABEL_DIR):
         init_label()
 
-    with open(SAVE_LABEL_DIR, 'rb') as f:
-        dataset = pickle.load(f)
+    with open(SAVE_LABEL_DIR, 'rb') as file:
+        dataset = pickle.load(file)
 
     return dataset
 #------------------------------------------------------
@@ -99,8 +100,8 @@ def init_test_img():
     dataset = _load_img(key_file['test_img'])
 
     print("Creating pickle file for test image...")
-    with open(SAVE_IMG_DIR, 'wb') as f:
-        pickle.dump(dataset, f, -1)#for later loading
+    with open(SAVE_IMG_DIR, 'wb') as file:
+        pickle.dump(dataset, file, -1)#for later loading
     print("Done!")
 
 def load_test_img():
@@ -113,12 +114,12 @@ def load_test_img():
     if not os.path.exists(SAVE_IMG_DIR):
         init_test_img()
 
-    with open(SAVE_IMG_DIR, 'rb') as f:
-        dataset = pickle.load(f)# slow
+    with open(SAVE_IMG_DIR, 'rb') as file:
+        dataset = pickle.load(file)# slow
 
     return dataset
 #------------------------------------------------------
-def conver_img_to_boolean(img_set, grayScale=73):
+def conver_img_to_boolean(img_set, gray_scale=73):
     """ Go through 60000x28x28 or 10000x28x28
         turn value 0-255 to 0-1
         Arg:
@@ -129,23 +130,23 @@ def conver_img_to_boolean(img_set, grayScale=73):
 
 
     """
-    set = []
+    new_img_set = []
     for img in img_set:
         new_img = img.reshape(28, 28)
         matrix = []
         for row in new_img:
-            list = []
-            for x in range(len(row)):
-                if row[x]<=grayScale:
-                    list.append(0)
+            ele = []
+            for index in range(len(row)):
+                if row[index]<=gray_scale:
+                    ele.append(0)
                 else:
-                    list.append(1)
-            matrix.append(list)
-        set.append(matrix)
+                    ele.append(1)
+            matrix.append(ele)
+        new_img_set.append(matrix)
 
-    return set
+    return new_img_set
 
-def init_img_boolean(grayScale=73):
+def init_img_boolean(gray_scale=73):
     """ Creating pickle file for img
         Save as {'train_img': ... , 'test_img': ...}
     """
@@ -154,30 +155,31 @@ def init_img_boolean(grayScale=73):
     train_img = _load_img(key_file['train_img'])
     test_img = _load_img(key_file['test_img'])
 
-    dataset ['train_img'] = conver_img_to_boolean(train_img,grayScale)
-    dataset ['test_img'] = conver_img_to_boolean(test_img,grayScale)
+    dataset ['train_img'] = conver_img_to_boolean(train_img,gray_scale)
+    dataset ['test_img'] = conver_img_to_boolean(test_img,gray_scale)
 
     print("Creating pickle file for image(boolean)...")
-    with open(SAVE_IMG_BOOLEAN_DIR, 'wb') as f:
-        pickle.dump(dataset, f, -1)#for later loading
+    with open(SAVE_IMG_BOOLEAN_DIR, 'wb') as file:
+        pickle.dump(dataset, file, -1)#for later loading
     print("Done!")
 
-def load_img_boolean(grayScale=73):
-    """ If pickle file not exist, load .gz, turn value 0-255 to 0-1 with grayscale and create pickle file.
+def load_img_boolean(gray_scale=73):
+    """ If pickle file not exist, load .gz,
+        turn value 0-255 to 0-1 with grayscale and create pickle file.
         Read pickle file.
         Returns:
             dataset: returns matrix 60000x28x28 or 10000x28x28 values are 0-1 depends on grayscale
     """
 
     if not os.path.exists(SAVE_IMG_BOOLEAN_DIR):
-        init_img_boolean(grayScale)
+        init_img_boolean(gray_scale)
 
-    with open(SAVE_IMG_BOOLEAN_DIR, 'rb') as f:
-        dataset = pickle.load(f)#takes 3s to load, which is slow
+    with open(SAVE_IMG_BOOLEAN_DIR, 'rb') as file:
+        dataset = pickle.load(file)#takes 3s to load, which is slow
 
     return dataset
 #------------------------------------------------------
-def conver_img_to_y_x(img_set,grayScale=73):
+def conver_img_to_y_x(img_set,gray_scale=73):
     """ Go through 60000x28x28 or 10000x28x28
         find grayscale which are greater than parameter
         append it to the list 60000 x unknow.
@@ -188,20 +190,18 @@ def conver_img_to_y_x(img_set,grayScale=73):
             set: dark pixel's locations in list 60000x28x28 [i,j] or 10000x28x28 [i,j]
 
     """
-    set = []
+    new_img_set = []
     for img in img_set:
         new_img = img.reshape(28, 28)
         matrix = []
         for y in range(28):
             for x in range(28):
-                if new_img[y][x]<=grayScale:
-                    continue
-                else:
+                if new_img[y][x]>gray_scale:
                     matrix.append((y,x))
-        set.append(matrix)
-    return set
+        new_img_set.append(matrix)
+    return new_img_set
 
-def init_img_to_y_x(grayScale=73):
+def init_img_to_y_x(gray_scale=73):
     """ Creating pickle file for img
         Save as {'train_img': ... , 'test_img': ...}
     """
@@ -210,42 +210,34 @@ def init_img_to_y_x(grayScale=73):
     train_img = _load_img(key_file['train_img'])
     test_img = _load_img(key_file['test_img'])
 
-    dataset ['train_img'] = conver_img_to_y_x(train_img, grayScale)
-    dataset ['test_img'] = conver_img_to_y_x(test_img, grayScale)
+    dataset ['train_img'] = conver_img_to_y_x(train_img, gray_scale)
+    dataset ['test_img'] = conver_img_to_y_x(test_img, gray_scale)
 
     print("Creating pickle file for image(index)...")
-    with open(SAVE_IMG_BOOLEAN_INDEX_DIR, 'wb') as f:
-        pickle.dump(dataset, f, -1)#for later loading
+    with open(SAVE_IMG_BOOLEAN_INDEX_DIR, 'wb') as file:
+        pickle.dump(dataset, file, -1)#for later loading
     print("Done!")
 
 
-def load_img_to_y_x_index(grayScale=73):
-    """ If pickle file not exist, load .gz, find location with grayscale and create pickle file.
+def load_img_to_y_x_index(gray_scale=73):
+    """ If pickle file not exist, load .gz,
+        find location with grayscale and create pickle file.
         Read pickle file.
         Returns:
-            dataset: returns matrix 60000x list of [i,j]  or 10000x list of [i,j] depends on grayscale
+            dataset: returns matrix 60000x list of [i,j]
+                or 10000x list of [i,j] depends on grayscale
     """
     if not os.path.exists(SAVE_IMG_BOOLEAN_INDEX_DIR):
-        init_img_to_y_x(grayScale)
+        init_img_to_y_x(gray_scale)
 
-    with open(SAVE_IMG_BOOLEAN_INDEX_DIR, 'rb') as f:
-        dataset = pickle.load(f)
-
-    # matrix = [['#']*28 for i in range(28)]
-    # value = dataset['train_img'][9]
-    # for q in value:
-    #     y = q[0]
-    #     x = q[1]
-    #     matrix[y][x] = '.'
-    # for w in matrix:
-    #     print(w)
-    # print(dataset['train_label'][9])
+    with open(SAVE_IMG_BOOLEAN_INDEX_DIR, 'rb') as file:
+        dataset = pickle.load(file)
 
     return dataset
 
 
 if __name__ == '__main__':
-    dataset = _load_label_pkl()
-    print(dataset['train_label'][0])
+    _dataset = _load_label_pkl()
+    print(_dataset['train_label'][0])
     load_img_boolean()
     load_img_to_y_x_index()#only value "1" location in the list
